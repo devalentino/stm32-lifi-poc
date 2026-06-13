722214522c391975656df962e8517f6c0254b7dd
devalentino <valentin.saulyak@gmail.com>
2026-06-13 13:53:12 +0300

    LiFi protocol implementation and partial tests coverage

--- Tests/integration/test_lifi_protocol.c
+++ Tests/integration/test_lifi_protocol.c
@@ -90,6 +90,34 @@ void test_transmit_payload__wrong_crc(void)
     TEST_ASSERT_EQUAL_UINT8(server_socket.rx_package_bytes_received, 0);
 }
 
+void test_transmit_payload__receiver_ignores_package_on_wrong_start_byte(void)
+{
+    LiFi_Transmitter_t client_transmitter = {0};
+    LiFi_Receiver_t client_receiver = {0};
+    LiFi_Socket_t client_socket = {0};
+
+    LiFi_Transmitter_t server_transmitter = {0};
+    LiFi_Receiver_t server_receiver = {0};
+    LiFi_Socket_t server_socket = {0};
+
+    LiFi_Socket_Init(&client_socket, &client_transmitter, &client_receiver);
+    LiFi_Socket_Init(&server_socket, &server_transmitter, &server_receiver);
+    Fake_LiFi_Link_Register(&client_transmitter, &server_receiver);
+    Fake_LiFi_Link_Register(&server_transmitter, &client_receiver);
+
+    uint8_t client_payload[] = {'H', 'i'};
+    uint8_t read_buffer[2] = {0};
+
+    LiFi_Socket_Read(&server_socket, read_buffer);
+    LiFi_Socket_Send(&client_socket, client_payload, sizeof(client_payload));
+
+    client_transmitter.tx_buffer[1] = 0;
+    Fake_LiFi_RunUntilIdle();
+
+    TEST_ASSERT_EQUAL_UINT8('0', read_buffer[0]);
+    TEST_ASSERT_EQUAL_UINT8('0', read_buffer[1]);
+}
+
 void test_socket_coontinue_transmission_after_confirmation(void)
 {
     LiFi_Transmitter_t client_transmitter = {0};
@@ -135,9 +163,10 @@ int main(void)
 {
     UNITY_BEGIN();
 
-    // RUN_TEST(test_transmit_payload);
+    RUN_TEST(test_transmit_payload);
     RUN_TEST(test_transmit_payload__wrong_crc);
-    // RUN_TEST(test_socket_coontinue_transmission_after_confirmation);
+    RUN_TEST(test_transmit_payload__receiver_ignores_package_on_wrong_start_byte);
+    RUN_TEST(test_socket_coontinue_transmission_after_confirmation);
 
     return UNITY_END();
 }
